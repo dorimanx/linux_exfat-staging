dd13eee5922da0dae891913b1a1b8a45b0c32fb8
Benjamin Valentin <benpicco@zedat.fu-berlin.de>
2013-08-26 16:04:49 +0200

    add exfat fs based on Samsung's GPLv2 exfat driver
    Samsung released this driver for the exFAT (and FAT12/16/32) filesystem
    under the GPLv2, it is based on a fork of the in-kernel FAT driver.
    
    This driver is based on version 1.2.4 of Samsungs OpenSource driver with
    fixes for later Kernel releases.
    
    original authors: Joosun Hahn
    		Sung-Kwan Kim
    
    Signed-off-by: Liu Qishuai <lqs@lqs.me>
    Signed-off-by: rxrz <rxrz@hush.ai>
    Signed-off-by: Dorimanx <yuri@bynet.co.il>
    Signed-off-by: Benjamin Valentin <benpicco@zedat.fu-berlin.de>

---
+++ drivers/staging/exfat/exfat_oal.h
@@ -0,0 +1,48 @@
+/*
+ *  Copyright (C) 2012-2013 Samsung Electronics Co., Ltd.
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef _EXFAT_OAL_H
+#define _EXFAT_OAL_H
+
+#include "exfat_global.h"
+#include <linux/version.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+	typedef struct {
+		UINT16      sec;        /* 0 ~ 59               */
+		UINT16      min;        /* 0 ~ 59               */
+		UINT16      hour;       /* 0 ~ 23               */
+		UINT16      day;        /* 1 ~ 31               */
+		UINT16      mon;        /* 1 ~ 12               */
+		UINT16      year;       /* 0 ~ 127 (since 1980) */
+	} TIMESTAMP_T;
+
+	INT32 sm_init(struct semaphore *sm);
+	INT32 sm_P(struct semaphore *sm);
+	void  sm_V(struct semaphore *sm);
+
+	TIMESTAMP_T *tm_current(TIMESTAMP_T *tm);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
